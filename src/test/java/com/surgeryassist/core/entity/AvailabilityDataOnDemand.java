79c25feef36e9253bb902278edf59227aa5fd076
Ankit Tyagi <ankit.tyagi@me.com>
2013-01-04 15:48:29 -0600

    adding more tests

--- src/test/java/com/surgeryassist/core/entity/AvailabilityDataOnDemand.java
+++ src/test/java/com/surgeryassist/core/entity/AvailabilityDataOnDemand.java
@@ -1,7 +1,126 @@
 package com.surgeryassist.core.entity;
 
-import org.springframework.roo.addon.dod.RooDataOnDemand;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
 
-@RooDataOnDemand(entity = DayAvailability.class)
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.stereotype.Component;
+
+import com.surgeryassist.core.entity.ApplicationUser;
+import com.surgeryassist.core.entity.DayAvailability;
+
+@Configurable
+@Component
 public class AvailabilityDataOnDemand {
+	
+	private Random rnd = new SecureRandom();
+
+	private List<DayAvailability> data;
+
+	@Autowired
+	ApplicationUserDataOnDemand applicationUserDataOnDemand;
+
+	public DayAvailability getNewTransientAvailability(int index) {
+		DayAvailability obj = new DayAvailability();
+		setCreatedBy(obj, index);
+		setCreatedDate(obj, index);
+		setDateOfAvailability(obj, index);
+		setModifiedBy(obj, index);
+		setModifiedDate(obj, index);
+		setUserId(obj, index);
+		return obj;
+	}
+
+	public void setCreatedBy(DayAvailability obj, int index) {
+		Integer createdBy = new Integer(index);
+		obj.setCreatedBy(createdBy);
+	}
+
+	public void setCreatedDate(DayAvailability obj, int index) {
+		Calendar createdDate = Calendar.getInstance();
+		obj.setCreatedDate(createdDate);
+	}
+
+	public void setDateOfAvailability(DayAvailability obj, int index) {
+		Calendar dateOfAvailability = Calendar.getInstance();
+		obj.setDateOfAvailability(dateOfAvailability);
+	}
+
+	public void setModifiedBy(DayAvailability obj, int index) {
+		Integer modifiedBy = new Integer(index);
+		obj.setModifiedBy(modifiedBy);
+	}
+
+	public void setModifiedDate(DayAvailability obj, int index) {
+		Calendar modifiedDate = Calendar.getInstance();
+		obj.setModifiedDate(modifiedDate);
+	}
+
+	public void setUserId(DayAvailability obj, int index) {
+		ApplicationUser userId = applicationUserDataOnDemand.getRandomApplicationUser();
+		obj.setUserId(userId);
+	}
+
+	public DayAvailability getSpecificAvailability(int index) {
+		init();
+		if (index < 0) {
+			index = 0;
+		}
+		if (index > (data.size() - 1)) {
+			index = data.size() - 1;
+		}
+		DayAvailability obj = data.get(index);
+		Integer id = obj.getAvailabilityID();
+		return DayAvailability.findDayAvailability(id);
+	}
+
+	public DayAvailability getRandomAvailability() {
+		init();
+		DayAvailability obj = data.get(rnd.nextInt(data.size()));
+		Integer id = obj.getAvailabilityID();
+		return DayAvailability.findDayAvailability(id);
+	}
+
+	public boolean modifyAvailability(DayAvailability obj) {
+		return false;
+	}
+
+	public void init() {
+		int from = 0;
+		int to = 10;
+		data = DayAvailability.findDayAvailabilityEntries(from, to);
+		if (data == null) {
+			throw new IllegalStateException("Find entries implementation for 'Availability' illegally returned null");
+		}
+		if (!data.isEmpty()) {
+			return;
+		}
+
+		data = new ArrayList<DayAvailability>();
+		for (int i = 0; i < 10; i++) {
+			DayAvailability obj = getNewTransientAvailability(i);
+			try {
+				obj.persist();
+			} catch (ConstraintViolationException e) {
+				StringBuilder msg = new StringBuilder();
+				for (Iterator<ConstraintViolation<?>> iter = e.getConstraintViolations().iterator(); iter.hasNext();) {
+					ConstraintViolation<?> cv = iter.next();
+					msg.append("[").append(cv.getConstraintDescriptor()).append(":").append(cv.getMessage()).append("=").append(cv.getInvalidValue()).append("]");
+				}
+				throw new RuntimeException(msg.toString(), e);
+			}
+			obj.flush();
+			data.add(obj);
+		}
+	}
+
+
 }
--- src/test/java/com/surgeryassist/core/entity/AvailabilityIntegrationTest.java
+++ src/test/java/com/surgeryassist/core/entity/AvailabilityIntegrationTest.java
@@ -1,4 +1,118 @@
 package com.surgeryassist.core.entity;
 
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.surgeryassist.core.entity.DayAvailability;
+import com.surgeryassist.core.entity.AvailabilityDataOnDemand;
+
+@Configurable
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/META-INF/spring/applicationContext*.xml")
 public class AvailabilityIntegrationTest {
+	
+	@Autowired
+	AvailabilityDataOnDemand dod;
+
+	@Test
+	public void testCountAvailabilitys() {
+		Assert.assertNotNull("Data on demand for 'Availability' failed to initialize correctly", dod.getRandomAvailability());
+		long count = DayAvailability.countDayAvailabilitys();
+		Assert.assertTrue("Counter for 'Availability' incorrectly reported there were no entries", count > 0);
+	}
+
+	@Test
+	public void testFindAvailability() {
+		DayAvailability obj = dod.getRandomAvailability();
+		Assert.assertNotNull("Data on demand for 'Availability' failed to initialize correctly", obj);
+		Integer id = obj.getAvailabilityID();
+		Assert.assertNotNull("Data on demand for 'Availability' failed to provide an identifier", id);
+		obj = DayAvailability.findDayAvailability(id);
+		Assert.assertNotNull("Find method for 'Availability' illegally returned null for id '" + id + "'", obj);
+		Assert.assertEquals("Find method for 'Availability' returned the incorrect identifier", id, obj.getAvailabilityID());
+	}
+
+	@Test
+	public void testFindAllAvailabilitys() {
+		Assert.assertNotNull("Data on demand for 'Availability' failed to initialize correctly", dod.getRandomAvailability());
+		long count = DayAvailability.countDayAvailabilitys();
+		Assert.assertTrue("Too expensive to perform a find all test for 'Availability', as there are " + count + " entries; set the findAllMaximum to exceed this value or set findAll=false on the integration test annotation to disable the test", count < 250);
+		List<DayAvailability> result = DayAvailability.findAllDayAvailabilitys();
+		Assert.assertNotNull("Find all method for 'Availability' illegally returned null", result);
+		Assert.assertTrue("Find all method for 'Availability' failed to return any data", result.size() > 0);
+	}
+
+	@Test
+	public void testFindAvailabilityEntries() {
+		Assert.assertNotNull("Data on demand for 'Availability' failed to initialize correctly", dod.getRandomAvailability());
+		long count = DayAvailability.countDayAvailabilitys();
+		if (count > 20) count = 20;
+		int firstResult = 0;
+		int maxResults = (int) count;
+		List<DayAvailability> result = DayAvailability.findDayAvailabilityEntries(firstResult, maxResults);
+		Assert.assertNotNull("Find entries method for 'Availability' illegally returned null", result);
+		Assert.assertEquals("Find entries method for 'Availability' returned an incorrect number of entries", count, result.size());
+	}
+
+	@Test
+	public void testFlush() {
+		DayAvailability obj = dod.getRandomAvailability();
+		Assert.assertNotNull("Data on demand for 'Availability' failed to initialize correctly", obj);
+		Integer id = obj.getAvailabilityID();
+		Assert.assertNotNull("Data on demand for 'Availability' failed to provide an identifier", id);
+		obj = DayAvailability.findDayAvailability(id);
+		Assert.assertNotNull("Find method for 'Availability' illegally returned null for id '" + id + "'", obj);
+		boolean modified =  dod.modifyAvailability(obj);
+		Integer currentVersion = obj.getVersion();
+		obj.flush();
+		Assert.assertTrue("Version for 'Availability' failed to increment on flush directive", (currentVersion != null && obj.getVersion() > currentVersion) || !modified);
+	}
+
+	@Test
+	public void testMergeUpdate() {
+		DayAvailability obj = dod.getRandomAvailability();
+		Assert.assertNotNull("Data on demand for 'Availability' failed to initialize correctly", obj);
+		Integer id = obj.getAvailabilityID();
+		Assert.assertNotNull("Data on demand for 'Availability' failed to provide an identifier", id);
+		obj = DayAvailability.findDayAvailability(id);
+		boolean modified =  dod.modifyAvailability(obj);
+		Integer currentVersion = obj.getVersion();
+		DayAvailability merged = obj.merge();
+		obj.flush();
+		Assert.assertEquals("Identifier of merged object not the same as identifier of original object", merged.getAvailabilityID(), id);
+		Assert.assertTrue("Version for 'Availability' failed to increment on merge and flush directive", (currentVersion != null && obj.getVersion() > currentVersion) || !modified);
+	}
+
+	@Test
+	public void testPersist() {
+		Assert.assertNotNull("Data on demand for 'Availability' failed to initialize correctly", dod.getRandomAvailability());
+		DayAvailability obj = dod.getNewTransientAvailability(Integer.MAX_VALUE);
+		Assert.assertNotNull("Data on demand for 'Availability' failed to provide a new transient entity", obj);
+		Assert.assertNull("Expected 'Availability' identifier to be null", obj.getAvailabilityID());
+		obj.persist();
+		obj.flush();
+		Assert.assertNotNull("Expected 'Availability' identifier to no longer be null", obj.getAvailabilityID());
+	}
+
+	@Test
+	public void testRemove() {
+		DayAvailability obj = dod.getRandomAvailability();
+		Assert.assertNotNull("Data on demand for 'Availability' failed to initialize correctly", obj);
+		Integer id = obj.getAvailabilityID();
+		Assert.assertNotNull("Data on demand for 'Availability' failed to provide an identifier", id);
+		obj = DayAvailability.findDayAvailability(id);
+		obj.remove();
+		obj.flush();
+		Assert.assertNull("Failed to remove 'Availability' with identifier '" + id + "'", DayAvailability.findDayAvailability(id));
+	}
+
 }
