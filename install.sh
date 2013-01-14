08cb321930f6a5ffe5d99beaed4e3fed3992a0c7
Ankit Tyagi <atyagi@dandb.com>
2013-01-13 19:38:18 -0800

    adding install script to add sqljdbc4.jar easier

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+# Installs necessary jar files into local maven repository
+# 
+# Note: change your user account name before running
+#
+USER = atyagi
+
+mvn install:install-file -Dfile=/Users/$USER/projects/SurgeryAssist/extraJars/sqljdbc4.jar -DgroupId=com.microsoft.sqlserver -DartifactId=sqljdbc4 -Dversion=3.0 -Dpackaging=jar
