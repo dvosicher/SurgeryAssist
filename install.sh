1f2ce6dc817c6163d41687c970631f9789ed9f84
Ankit Tyagi <ankit.tyagi@me.com>
2013-01-13 19:55:59 -0800

    fixing install script

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 # 
 # Note: change your user account name before running
 #
-USER = atyagi
+USER=atyagi
 
 mvn install:install-file -Dfile=/Users/$USER/projects/SurgeryAssist/extraJars/sqljdbc4.jar -DgroupId=com.microsoft.sqlserver -DartifactId=sqljdbc4 -Dversion=3.0 -Dpackaging=jar
