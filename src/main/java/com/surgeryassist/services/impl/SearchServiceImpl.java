6944850e6dc68e4c18082bd6431eb4a093e07c26
Ankit Tyagi <atyagi@dandb.com>
2013-02-24 18:53:37 -0800

    setting up the base for search to work

--- src/main/java/com/surgeryassist/services/impl/SearchServiceImpl.java
+++ src/main/java/com/surgeryassist/services/impl/SearchServiceImpl.java
@@ -1,13 +1,23 @@
 package com.surgeryassist.services.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.faces.model.SelectItem;
 
 import org.springframework.stereotype.Service;
 
+import com.surgeryassist.core.SearchType;
 import com.surgeryassist.core.entity.DayAvailability;
 import com.surgeryassist.services.interfaces.SearchService;
 
+/**
+ * Implementation of {@link SearchService}
+ * @see SearchService
+ * @author Nick Karpov
+ */
 @Service("searchService")
 public class SearchServiceImpl implements SearchService {
 
@@ -17,25 +27,39 @@ public class SearchServiceImpl implements SearchService {
 		
 		if (result != null)
 			return result;
-		return new ArrayList<DayAvailability> ();
+		return new ArrayList<DayAvailability>();
 	}
 
 	@Override
 	public List<DayAvailability> searchByCity(String city) {
 		List<DayAvailability> result = DayAvailability.findAllDayAvailabilitysByCity(city);
 		
-		if (result != null)
+		if (result != null) {
 			return result;
-		return new ArrayList<DayAvailability> ();
+		}
+		return new ArrayList<DayAvailability>();
 	}
 
 	@Override
 	public List<DayAvailability> searchByZipCode(Integer zipCode) {
 		List<DayAvailability> result = DayAvailability.findAllDayAvailabilitysByZipCode(zipCode);
 		
-		if (result != null)
+		if (result != null) {
 			return result;
-		return new ArrayList<DayAvailability> ();
+		}
+		return new ArrayList<DayAvailability>();
+	}
+	
+	public Map<String, List<SelectItem>> getSelectItemValues() {
+		Map<String, List<SelectItem>> mapOfSelectItems = new HashMap<String, List<SelectItem>>();
+		List<SelectItem> searchTypeSelectList = new ArrayList<SelectItem>();
+		
+		for(SearchType searchType : SearchType.values()) {
+			searchTypeSelectList.add(new SelectItem(searchType, searchType.toString()));
+		}
+		mapOfSelectItems.put("searchType", searchTypeSelectList);
+		
+		return mapOfSelectItems;
 	}
 
 }
--- src/main/java/com/surgeryassist/services/interfaces/SearchService.java
+++ src/main/java/com/surgeryassist/services/interfaces/SearchService.java
@@ -1,10 +1,36 @@
 package com.surgeryassist.services.interfaces;
 
+import java.util.ArrayList;
 import java.util.List;
 import com.surgeryassist.core.entity.DayAvailability;
 
+/**
+ * Allows for searching for different records
+ * based on parameters and passed in values.
+ * 
+ * @author Nick Karpov
+ */
 public interface SearchService {
-	public List<DayAvailability> searchAll ();
-	public List<DayAvailability> searchByCity (String city);
+	
+	/**
+	 * Returns a dump of all day availabilities
+	 * @return {@link ArrayList} of {@link DayAvailability}
+	 */
+	public List<DayAvailability> searchAll();
+	
+	/**
+	 * Returns a list of {@link DayAvailability}
+	 * for a specific city (fuzzy search)
+	 * @param city String of city name
+	 * @return {@link ArrayList} of {@link DayAvailability}
+	 */
+	public List<DayAvailability> searchByCity(String city);
+	
+	/**
+	 * Returns a list of {@link DayAvailability}
+	 * for a zip code (fuzzy search)
+	 * @param zipCode Integer of the zipcode
+	 * @return {@link ArrayList} of {@link DayAvailability}
+	 */
 	public List<DayAvailability> searchByZipCode(Integer zipCode);
 }
